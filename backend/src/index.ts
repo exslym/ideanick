67fcd3f169690465941339400e1b34759f948dfd
exslym <76844562+exslym@users.noreply.github.com>
2024-12-28 20:48:54 +0100

    add tRPC

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,19 @@
+import * as trpcExpress from '@trpc/server/adapters/express';
 import express from 'express';
+import { trpcRouter } from './trpc';
 
 const expressApp = express();
 
 expressApp.get('/ping', (req, res) => {
   res.send('pong');
 });
+
+expressApp.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: trpcRouter,
+  }),
+);
 
 expressApp.listen(3000, () => {
   console.info('Listening at http://localhost:3000');
