2693e9dbfe1215e2d5a869bdfcaeb4fb141b35e7
exslym <76844562+exslym@users.noreply.github.com>
2025-01-29 10:33:57 +0100

    add lodash

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,39 +1,19 @@
 import { initTRPC } from '@trpc/server';
+import _ from 'lodash';
 
-const ideas = [
-  {
-    nick: 'cool-idea-nick-1',
-    name: 'Idea 1',
-    description: 'Idea 1 description...',
-  },
-  {
-    nick: 'cool-idea-nick-2',
-    name: 'Idea 2',
-    description: 'Idea 2 description...',
-  },
-  {
-    nick: 'cool-idea-nick-3',
-    name: 'Idea 3',
-    description: 'Idea 3 description...',
-  },
-  {
-    nick: 'cool-idea-nick-4',
-    name: 'Idea 4',
-    description: 'Idea 4 description...',
-  },
-  {
-    nick: 'cool-idea-nick-5',
-    name: 'Idea 5',
-    description: 'Idea 5 description...',
-  },
-];
+const ideas = _.times(100, (i) => ({
+  nick: `cool-idea-nick-${i}`,
+  name: `Idea ${i}`,
+  description: `Description of idea ${i}...`,
+  text: _.times(100, (j) => `<p>Text paragraf ${j} of idea ${i}...</p>`).join(''),
+}));
 
 export const trpc = initTRPC.create();
 
 export const trpcRouter = trpc.router({
   getIdeas: trpc.procedure.query(() => {
     return {
-      ideas,
+      ideas: ideas.map((idea) => _.pick(idea, ['nick', 'name', 'description'])),
     };
   }),
 });
