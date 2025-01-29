91852d64ffa080d516650907fc6ffda210d4636a
exslym <76844562+exslym@users.noreply.github.com>
2025-01-29 15:40:25 +0100

    Updated with zod validation

--- webapp/src/pages/ideas/NewIdeaPage/index.tsx
+++ webapp/src/pages/ideas/NewIdeaPage/index.tsx
@@ -1,7 +1,10 @@
 import { useFormik } from 'formik';
+import { withZodSchema } from 'formik-validator-zod';
+import { z } from 'zod';
 import { Input } from '../../../components/Input';
 import { Segment } from '../../../components/Segment';
 import { Textarea } from '../../../components/Textarea';
+import css from './index.module.scss';
 
 export const NewIdeaPage = () => {
   const formik = useFormik({
@@ -11,31 +14,17 @@ export const NewIdeaPage = () => {
       description: '',
       text: '',
     },
-    validate: (values) => {
-      const errors: Partial<typeof values> = {};
-
-      if (!values.name) {
-        errors.name = 'Name is required';
-      }
-
-      if (!values.nick) {
-        errors.nick = 'Nick is required';
-      } else if (!values.nick.match(/^[a-z0-9-]+$/)) {
-        errors.nick = 'Nick can only contain lowercase letters, numbers and dashes';
-      }
-
-      if (!values.description) {
-        errors.description = 'Description is required';
-      }
-
-      if (!values.text) {
-        errors.text = 'Text is required';
-      } else if (values.text.length < 100) {
-        errors.text = 'Text should be at least 100 characters';
-      }
-
-      return errors;
-    },
+    validate: withZodSchema(
+      z.object({
+        name: z.string().min(1, 'Name is required'),
+        nick: z
+          .string()
+          .min(1, 'Nick is required')
+          .regex(/^[a-z0-9-]+$/, 'Nick can only contain lowercase letters, numbers and dashes'),
+        description: z.string().min(1, 'Description is required'),
+        text: z.string().min(1, 'Text should be at least 100 characters'),
+      }),
+    ),
     onSubmit: (values) => {
       console.info('Submitted', values);
     },
@@ -53,7 +42,7 @@ export const NewIdeaPage = () => {
         <Input name='nick' label='Nick' formik={formik} />
         <Input name='description' label='Description' formik={formik} />
         <Textarea name='text' label='Text' formik={formik} />
-
+        {!formik.isValid && !!formik.submitCount && <div className={css.error}>Some fields are invalid</div>}
         <button type='submit'>Create Idea</button>
       </form>
     </Segment>
