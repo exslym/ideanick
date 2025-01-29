9ae705bb0697a3bf6288c0519581e7f96831998f
exslym <76844562+exslym@users.noreply.github.com>
2025-01-29 09:56:57 +0100

    add routes

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { getAllIdeasRoute, getViewIdeaRoute } from './lib/routes';
 import { TRPCProvider } from './lib/trpc';
 import { AllIdeasPage } from './pages/AllIdeasPage';
+import { ViewIdeaPage } from './pages/ViewIdeaPage';
 
 export const App = () => {
   return (
     <TRPCProvider>
-      <AllIdeasPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
+          <Route path={getViewIdeaRoute({ ideaNick: ':ideaNick' })} element={<ViewIdeaPage />} />
+        </Routes>
+      </BrowserRouter>
     </TRPCProvider>
   );
 };
---
+++ webapp/src/pages/ViewIdeaPage/index.tsx
@@ -0,0 +1,17 @@
+import { useParams } from 'react-router-dom';
+
+export const ViewIdeaPage = () => {
+  const { ideaNick } = useParams() as { ideaNick: string };
+
+  return (
+    <div>
+      <h1>{ideaNick}</h1>
+      <p>Description of Idea 1...</p>
+      <div>
+        <p>Text paragraf 1 of Idea 1...</p>
+        <p>Text paragraf 2 of Idea 1...</p>
+        <p>Text paragraf 3 of Idea 1...</p>
+      </div>
+    </div>
+  );
+};
