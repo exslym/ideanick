c14a37c67dbb3d2bcb44e23f6cae19c9b9e78d88
exslym <76844562+exslym@users.noreply.github.com>
2025-01-29 15:24:11 +0100

    Add simple validation

--- webapp/src/pages/ideas/NewIdeaPage/index.tsx
+++ webapp/src/pages/ideas/NewIdeaPage/index.tsx
@@ -11,8 +11,33 @@ export const NewIdeaPage = () => {
       description: '',
       text: '',
     },
+    validate: (values) => {
+      const errors: Partial<typeof values> = {};
+
+      if (!values.name) {
+        errors.name = 'Name is required';
+      }
+
+      if (!values.nick) {
+        errors.nick = 'Nick is required';
+      } else if (!values.nick.match(/^[a-z0-9-]+$/)) {
+        errors.nick = 'Nick can only contain lowercase letters, numbers and dashes';
+      }
+
+      if (!values.description) {
+        errors.description = 'Description is required';
+      }
+
+      if (!values.text) {
+        errors.text = 'Text is required';
+      } else if (values.text.length < 100) {
+        errors.text = 'Text should be at least 100 characters';
+      }
+
+      return errors;
+    },
     onSubmit: (values) => {
-      console.info(values);
+      console.info('Submitted', values);
     },
   });
 
